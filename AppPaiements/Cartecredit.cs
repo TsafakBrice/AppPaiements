052e970a94f0a59bd0be2ae2be0d3d677b5eede2
BRICE <romainbrice.tsafakngou@monboreal.ca>
2024-10-24 20:14:18 -0400

    ajout2 fait

---
+++ AppPaiements/Cartecredit.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class Cartecredit : Paiement
+    {
+
+        public int NumeroCarte { get; set; }
+
+        public Cartecredit(double montant, string description, int numerocarte) : base(montant, description)
+
+        {
+
+            NumeroCarte = numerocarte;
+
+        }
+
+
+        public override void AfficherDetails()
+        {
+            base.AfficherDetails();
+            Console.WriteLine($"le numero de la carte est {NumeroCarte}");
+
+
+        }
+
+    
+}
+}
--- AppPaiements/Program.cs
+++ AppPaiements/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AppPaiements;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+      
+        
+        Console.WriteLine("Hello, World!");
+
+        Cartecredit carteA = new Cartecredit(numerocarte: 111, description: "credit1", montant:1000);
+        Cartecredit carteB = new Cartecredit(numerocarte: 222, description: "credit2", montant: 2000);
+        Cartecredit carteC = new Cartecredit(numerocarte: 333, description: "credit3", montant: 3000);
+
+        carteA.AfficherDetails();
+        carteB.AfficherDetails();
+        carteC.AfficherDetails();
+    }
+}
+
+
+
