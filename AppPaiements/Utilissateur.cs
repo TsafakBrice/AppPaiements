060247250b95530b7cf91113b1a014bf327f9be8
BRICE <romainbrice.tsafakngou@monboreal.ca>
2024-10-24 20:46:29 -0400

    ajout4 fait

---
+++ AppPaiements/Utilissateur.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class Utilissateur
+    {
+        public string Nom { get; set; }
+        public List<Paiement> paiements { get; set; }
+
+        public Utilissateur(string nom)
+        {
+
+            Nom = nom;
+            paiements = new List<Paiement>();
+
+
+        }
+
+        public void AfficherInfos()
+
+        {
+            Console.WriteLine($"nom d'ulilisateur est {Nom}");
+            Console.WriteLine($"Liste de paiement :");
+            foreach (Paiement p in paiements)
+            {
+                Console.Write("....");
+                p.AfficherDetails();
+
+
+            }
+
+        }
+        public void AjouterPaiement(Paiement paiement)
+        {
+            paiements.Add(paiement);
+
+
+        }
+
+
+
+
+    }
+}
